8015ee8accf3ff26bca88606e2a58fb64c587840
alexander sampson <a.sampson206@gmail.com>
2023-10-20 15:50:24 -0700

    lol

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const init = () => {
-  
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit", (event) => {});
+    event.preventDefault();
 }
 
 document.addEventListener('DOMContentLoaded', init);
